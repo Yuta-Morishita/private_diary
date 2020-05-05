d42f1d9e6c1acfe202f199edd0e0ba34fa914810
uuta <yuta.morishita0916gmail.com>
2020-05-05 19:41:18 +0900

    split settings

---
+++ private_diary/settings_dev.py
@@ -0,0 +1,45 @@
+from .settings_common import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+LOGGING = {
+    'version': 1,  # 固定
+    'disable_existing_loggers': False,
+
+    # ロガーの設定
+    'loggers': {
+        # Djangoが利用するロガー
+        'django': {
+            'handler': ['cosole'],
+            'level': 'INFO',
+        },
+        # diaryアプリケーションが利用するロガー
+        'diary': {
+            'handler': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+    # ハンドラの設定
+    'handler': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'dev'
+        },
+    },
+    # フォーマッタの設定
+    'formatter': {
+        'dev': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        }
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
