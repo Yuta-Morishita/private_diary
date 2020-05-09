129cb4b0ce413ff76ab4cf2fa6b3e8f3e7a236bf
uuta <yuta.morishita0916gmail.com>
2020-05-09 19:17:17 +0900

    selenium test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from django.urls import reverse_lazy
+from selenium.webdriver.chrome.webdriver import WebDriver
 
-# Create your tests here.
+
+class TestLogin(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver(executable_path='/Users/yutamorishita/chromedriver')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login(self):
+        # ログインページを開く
+        self.selenium.get('http://localhost:8000' + str(reverse_lazy('account_login')))
+
+        # ログイン
+        username_input = self.selenium.find_element_by_name("login")
+        username_input.send_keys('admin@test.com')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('12345678q')
+        self.selenium.find_element_by_class_name('btn').click()
